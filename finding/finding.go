e0894b4d2138364a7233b69f26f3a4b9af72b8d9
Adriano <adriano.selaviles@gmail.com>
2019-07-16 15:03:06 -0700

    add some finding type generics

--- finding/finding.go
+++ finding/finding.go
@@ -6,3 +6,29 @@ type Finding interface {
 	GetUID() string
 	GetType() string
 }
+
+const (
+	// FindingTypePII is the finding type for findings related to
+	// user personally identifiable info such as email, phone number,
+	// home address, etc
+	FindingTypePII = "PersonallyIdentifiableInformation"
+
+	// FindingTypeDigitalAlias is the finding type for findings related to
+	// social media accounts and online profiles
+	FindingTypeDigitalAlias = "DigitalAlias"
+
+	// FindingTypeImage is the finding type for relevant image URLs
+	FindingTypeImage = "Event"
+
+	// FindingTypeEventDetails is the finding type for dated/timestamped
+	// events such as a public payment on Venmo
+	FindingTypeEventDetails = "Event"
+
+	// FindingTypeGeolocation is the finding type for events whose main
+	// feature is geophysical coordinates or other location types
+	FindingTypeGeolocation = "Geolocation"
+
+	// FindingTypeGenericData is the finding type for findings which do not
+	// fall into a more appropriate category
+	FindingTypeGenericData = "GenericData"
+)
